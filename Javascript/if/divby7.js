ab3617ce9c1f00b483a438c3054b2f0b7df00ac5
Jayanta garu <jayanta.garu@gmail.com>
2021-05-28 20:52:56 +0530

    divi

--- Javascript/if/divby7.js
+++ Javascript/if/divby7.js
@@ -1,8 +1,10 @@
-let value=22;
-if("value % 7 = 0") {
-    console.log("Number is divisible by 7");
-}
-else{
-    console.log("Number is not divisible by 7");
+let value=20;
+// if(value % 7 == 0) {
+//     console.log("Number is divisible by 7");
+// }
+// else{
+//     console.log("Number is not divisible by 7");
 
-}
+// }
+
+value % 7==0 ? console.log("Number is divisible by 7"): console.log("Number is not divisible by 7");
---
+++ Javascript/if/multiby3.js
@@ -0,0 +1,11 @@
+let value=23;
+
+// if(value % 3 == 0){
+//     console.log("Number is Multiple by 3");
+// }
+// else{
+// console.log("Number is not Multiple by 3");
+// }
+
+
+value % 3==0 ? console.log("Number is Multiple by 3"):console.log("Number is not Multiple by 3");
