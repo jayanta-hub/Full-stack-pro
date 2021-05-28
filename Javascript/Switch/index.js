5cd0d9e36c7de3bdc5d6bb9cdc64e5aa845559c2
Jayanta garu <jayanta.garu@gmail.com>
2021-05-28 19:56:28 +0530

    switch

--- Javascript/Switch/index.js
+++ Javascript/Switch/index.js
@@ -1,3 +1,22 @@
-switch{
-    
+let day="Thusday";
+switch (day){
+    case("Monday"):
+    console.log("Today is Monday");
+    break;
+    case("Tuesday"):
+    console.log("Today is Tuesday");
+    break;
+    case("Wednesday"):
+    console.log("Today is Wednesday");
+    break;
+    case("Thusday"):
+    console.log("Today is Thusday");
+    break;
+    case("Friday"):
+    console.log("Today is Friday");
+    break;
+    case("Saturday"):
+    console.log("Today is Saturday");
+    default:
+    console.log("Today is Sunday");
 }
