4c18633245101dc9c4aa7d653ed8f2d6c6948b83
Jayanta garu <jayanta.garu@gmail.com>
2021-05-29 18:18:40 +0530

    decending

--- Javascript/if/decending.js
+++ Javascript/if/decending.js
@@ -3,38 +3,40 @@ if ((x > y) && (x > z)) {
     l = x;
 }
 
-else {if ((x < y) && (x > z) || (x>y) && (x <z)){ 
-    m = x;
-}
+else {
+    if ((x < y) && (x > z) || (x > y) && (x < z)) {
+        m = x;
+    }
 
-if ((x < z) && (x < z)) {
-    s = x;
-}
+    if ((x < z) && (x < z)) {
+        s = x;
+    }
 }
 if ((y > z) && (y > x)) {
     l = y;
 }
 
-else {if ((y < z) && (y > x) || (y>z) && (y <x)){ 
-    m = y;
-}
+else {
+    if ((y < z) && (y > x) || (y > z) && (y < x)) {
+        m = y;
+    }
 
-if ((y < z) && (y < x)) {
-    s = y;
-}
+    if ((y < z) && (y < x)) {
+        s = y;
+    }
 }
 if ((z > x) && (z > y)) {
     l = z;
 }
 
-else
- {if ((z < x) && ( z > y) || (z>x) && (z <y)){ 
-    m = z;
+else {
+    if ((z < x) && (z > y) || (z > x) && (z < y)) {
+        m = z;
+    }
+
+    if ((z < x) && (z < y)) {
+        s = z;
+    }
 }
 
-if ((z < x) &&( z < y)) {
-    s = z;
-}
- }
-
-console.log("Ascending number is ",l, m, s, ".");
+console.log("Decending number is ", l, m, s, ".");
