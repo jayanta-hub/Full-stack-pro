039754c174043aa693f7333988ce21ebe3a7bf57
Jayanta garu <jayanta.garu@gmail.com>
2021-05-31 08:40:43 +0530

    odd

--- Javascript/if_for_while/Assingment_do_while/odd.js
+++ Javascript/if_for_while/Assingment_do_while/odd.js
@@ -1,3 +1,15 @@
-for (let i=1; i<=10; i=i+2){
+// for (let i=1; i<=10; i=i+2){
+//     console.log(i);
+// }
+
+let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i=i+2;
+// }
+
+do{
     console.log(i);
+    i=i+2;
 }
+while(i<=10);
