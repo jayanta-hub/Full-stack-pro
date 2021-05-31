c1cc936c9e70c58ae9cfb8d6f88bf9de3dcc24fc
Jayanta garu <jayanta.garu@gmail.com>
2021-05-31 08:21:59 +0530

    even

---
+++ Javascript/if/Assingment_do_while/even.js
@@ -0,0 +1,15 @@
+// for (let i=2; i<=10; i=i+2){
+//     console.log(i);
+// }
+
+let i=2;
+// while(i<=10){
+//     console.log(i);
+//     i=i+2;
+// }
+
+do{
+    console.log(i);
+    i=i+2;
+}
+while(i<=10);
