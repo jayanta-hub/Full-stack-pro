cfa887165f216c3a3e8cfc5051932bd1bd61145e
Jayanta garu <jayanta.garu@gmail.com>
2021-05-31 08:13:25 +0530

    5-15

---
+++ Javascript/if/Assingment_do_while/5to15.js
@@ -0,0 +1,15 @@
+// for(let i=5; i<=15; i++){
+//     console.log(i);
+// }
+
+let i=5;
+// while(i<=15){
+//     console.log(i);
+//     i++;
+// }
+
+do{
+    console.log(i);
+    i++;
+}
+while(i<=15);
