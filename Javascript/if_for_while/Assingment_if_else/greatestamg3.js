367dc77c076a0381e833cafaa9b554d0e06e9f7a
Jayanta garu <jayanta.garu@gmail.com>
2021-05-31 09:48:51 +0530

    commit

--- Javascript/if_for_while/Assingment_if_else/greatestamg3.js
+++ Javascript/if_for_while/Assingment_if_else/greatestamg3.js
@@ -1,22 +1,22 @@
-let x = 121;
+let x = 11;
 let y = 564;
-let z = 361;
+let z = 615;
 let max;
-// if (x > y && x > z){
-//     console.log("Greatest number  is :" , x);
-// }  
-// else if (y > z){
-//         console.log("Greatest number is :" , y);
-//         }
-// else{
+if ((x > y) && (x > z)){
+    console.log("Greatest number  is :" , x);
+}  
+else if ((y > z) && (y > x)){
+        console.log("Greatest number is :" , y);
+        }
+else{
 
-//         console.log("Greatest number is :" , z);
-//     }
+        console.log("Greatest number is :" , z);
+    }
 
 
-max = (x > y) ? (y > z ? x : z) : (y > z ? y : z);
+// max = (x > y) ? (y > z ? x : z) : (y > z ? y : z);
 
-console.log("Largest number among "
-    , x, ", ", y, " and "
-    , z, " is ", max, ".");
+// console.log("Largest number among "
+//     , x, ", ", y, " and "
+//     , z, " is ", max, ".");
 
