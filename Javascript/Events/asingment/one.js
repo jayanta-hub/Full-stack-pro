5bc66572ba4dca5312d57c8beb7ed626fc585cf1
Jayanta garu <jayanta.garu@gmail.com>
2021-06-05 21:11:18 +0530

    js

---
+++ Javascript/Events/asingment/one.js
@@ -0,0 +1,24 @@
+function mouseOver() {
+    document.getElementById("onmouseove").style.background = "red";
+    }
+
+  function mouseOut() {
+    document.getElementById("onmouseout").style.background = "red";
+    }
+
+   function onclick() {
+    document.getElementById("onclick").style.background = "red";
+    }
+
+  function mousedoubleclick() {
+    document.getElementById("onbdlclick").style.background = "red";
+    }
+ 
+    function myFunction(y) {
+      y.style.background = "yellow";
+    }
+
+    function myFunction() {
+        var x = document.getElementById("fname");
+        x.value = x.value.toUpperCase();
+      }
