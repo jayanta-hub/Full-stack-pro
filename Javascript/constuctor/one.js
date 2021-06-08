0040fb0d6f392859c9d57717a4044a6f9fa9c7e9
Jayanta garu <jayanta.garu@gmail.com>
2021-06-08 20:41:46 +0530

    constructor

---
+++ Javascript/constuctor/one.js
@@ -0,0 +1,10 @@
+class employee{
+    Name= "Jayanta Garu";
+    Emp_Id= 101;
+    Loc= "Odisha";
+    Position= "Full stsck develper";
+}
+// console.log(new employee());
+// console.log(new employee().Loc);
+let x= new employee();
+console.log(x.Name);
