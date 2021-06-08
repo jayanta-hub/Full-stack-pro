3ec5857ccfa0dcc63c1b0c04003f0505fe066b74
Jayanta garu <jayanta.garu@gmail.com>
2021-06-08 19:57:47 +0530

    object

---
+++ Javascript/object/one.js
@@ -0,0 +1,9 @@
+let emp= {
+    name: "Jayanta garu",
+    id: 101,
+    email:"jayanta.garu@gmail.com",
+    loc: 'odisha',
+    sal: 52000,
+}
+console.log(emp.name);
+console.log("Employee mail is" ,emp.email);
