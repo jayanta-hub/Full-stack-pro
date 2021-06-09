69c59bc24bb44162203858b9e2558d7fafaaa5ce
Jayanta garu <jayanta.garu@gmail.com>
2021-06-09 19:31:00 +0530

    wc

---
+++ Javascript/constructor/cstmr_address.js
@@ -0,0 +1,11 @@
+class address{
+    constructor(house_no,city,state,pin){
+        this.house_no=house_no;
+        this.city=city;
+        this.state=state;
+        this.pin=pin;
+    }
+}
+let add=new address(420,"jaleswar","Odisha",756035);
+console.log(add);
+Module.exports=address;
