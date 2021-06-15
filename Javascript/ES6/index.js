b82862595189480b5d41b66898726e9d78e79615
Jayanta garu <jayanta.garu@gmail.com>
2021-06-15 11:22:54 +0530

    cj

--- Javascript/ES6/index.js
+++ Javascript/ES6/index.js
@@ -1,5 +1,11 @@
-let employee = (a,b,c,d,)=>{
-    console.log(a, b, c,d);
-}
-   let emp=employee(101,"jayanta","odisha",50000);
-    console.log(emp);
+// let employee = (a,b,c,d,)=>{
+//     console.log(a, b, c,d);
+// }
+//    let emp=employee(101,"jayanta","odisha",50000);
+//     console.log(emp);
+
+let a=[1,2,3,4];
+let b={name:"jayanta"};
+
+let c= {...b ,...a};
+console.log(c);
