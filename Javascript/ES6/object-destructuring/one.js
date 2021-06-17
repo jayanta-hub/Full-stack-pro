fcb7030d22e5b1d5acc1251e767ceafc9617afec
Jayanta garu <jayanta.garu@gmail.com>
2021-06-17 10:01:49 +0530

    es6

--- Javascript/ES6/object-destructuring/one.js
+++ Javascript/ES6/object-destructuring/one.js
@@ -1,4 +1,7 @@
-let emp={name:"jayanta",age:27,
-address:{city:"njd", st:"od"}}
-let {address:{st}}=emp;
-console.log(st);
+// let emp={name:"jayanta",age:27,
+// address:{city:"njd", st:"od"}}
+// let {address:{st}}=emp;
+// console.log(st);
+
+let emp_details = {name:"jayanta",age:25};
+console.log(emp_details.name, emp_details.age );
