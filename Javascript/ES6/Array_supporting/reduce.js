0c519215c7f5ce8e74248f415594c849d8ff6222
Jayanta garu <jayanta.garu@gmail.com>
2021-06-18 10:13:24 +0530

    reduce

---
+++ Javascript/ES6/Array_supporting/reduce.js
@@ -0,0 +1,19 @@
+let arr = [1, 2, 3];
+let result = arr.reduce((total) => {
+  console.log(total, ".....");
+  return total;
+});
+
+console.log(result);
+
+arr.map((value, index)=>{
+    return <tr>
+        <td>{value}</td>
+        <td>{value}</td>
+        <td>{}</td>
+        <td>{}</td>
+    </tr>
+})
+
+arr.forEach( function(e){});
+arr.forEach( ()=>{});
