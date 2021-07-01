016800a50f4b0a8811c1ebae817d86566f2faad9
Jayanta garu <jayanta.garu@gmail.com>
2021-07-01 16:12:56 +0530

    state

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,11 @@
 import "./assets/bootstrap.css"
 // import CB from "./components/Class-Class/CB.jsx"
-import Product from "./components/Product/Product.jsx"
+import Product1 from "./components/Product/Product1.jsx"
 function App() {
   return (
     <div >
       {/* <CB/> */}
-      <Product/>
+      <Product1/>
     </div>
   );
 }
--- reactjs/src/components/Product/Product1.jsx
+++ reactjs/src/components/Product/Product1.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 class Product extends React.Component {
-  state = {
-    product: {
-      Dog_Name: "German Shepherd",
-      Price:50000
-    }
-  };
+  state =[
+      "Dog_Name: German Shepherd",
+      "Price:50000",
+      
+    ];
   change_product_name = () => {
-    this.setState({product:{ 
-      ...this.state.product,
-      Dog_Name:'German'
-      }});
+    this.setState([
+      ...this.state,[1]="Dog_Name: German"
+    ]);
   };
   render() {
     return (
@@ -20,10 +18,10 @@ class Product extends React.Component {
             <div className="col-md-6">
               <div className="card">
                 <div className="card-header">
-                  <pre>{JSON.stringify(this.state.product)}</pre>
-                  <h4>Price:{this.state.product.Price}</h4>
+                  <pre>{JSON.stringify(this.state)}</pre>
+                  <h4>Price:{this.state[1]}</h4>
                   <div className="card-body">
-                    <h5>Dog Name: {this.state.product.Dog_Name}</h5>
+                    <h5>Dog Name: {this.state[0]}</h5>
                     <button
                       className="btn btn-primary"
                       onClick={this.change_product_name}
