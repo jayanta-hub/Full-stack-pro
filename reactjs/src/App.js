e2bd12c9fe370bfd30216eb1ae405722b25df60c
Jayanta garu <jayanta.garu@gmail.com>
2021-07-08 21:52:52 +0530

    eventhandling

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -2,12 +2,14 @@ import "./assets/bootstrap.css"
 import Login from "./FormHandling/Login/Login.jsx"
 import Registration from "./FormHandling/Registration/regi.jsx"
 import Singin from "./FormHandling/Singin/Singin.jsx"
+import SalaryUI from "./Event-handling/SalaryUI"
 function App() {
   return (
     <div >
       <Login/>
       <Registration/>
       <Singin/>
+      <SalaryUI/>
     </div>
   );
 }
