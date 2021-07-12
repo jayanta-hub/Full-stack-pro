bbf93e7720b9a48c3cb8566515d139e370c4220e
Jayanta garu <jayanta.garu@gmail.com>
2021-07-12 12:16:00 +0530

    Rendering

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,15 +1,19 @@
 import "./assets/bootstrap.css"
-import Login from "./FormHandling/Login/Login.jsx"
+// import Login from "./FormHandling/Login/Login.jsx"
 import Registration from "./FormHandling/Registration/regi.jsx"
-import Singin from "./FormHandling/Singin/Singin.jsx"
+// import Singin from "./FormHandling/Singin/Singin.jsx"
 import SalaryUI from "./Event-handling/SalaryUI"
+import Signup from "./FormHandling/Singup/Singup"
+import Employee from "./Render/employee"
 function App() {
   return (
     <div >
-      <Login/>
+      {/* <Login/> */}
       <Registration/>
-      <Singin/>
+      {/* <Singin/> */}
       <SalaryUI/>
+      <Signup/>
+      <Employee/>
     </div>
   );
 }
