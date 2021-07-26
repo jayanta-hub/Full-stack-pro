b548bc075d1ea42a59c46c280de3be5f66a57115
Jayanta garu <jayanta.garu@gmail.com>
2021-07-26 09:38:43 +0530

    redux

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,22 @@
-import "./assets/bootstrap.css"
-import CovidData from "./Covid-details/CovidData";
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./Hooks/Component/about.jsx";
+import Navbar from "./Hooks/NavBar/Navbar.jsx";
+import ContactApp from "./Hooks/ContactApp/contactapp.jsx";
+import DigitalClock from "./Hooks/DigitalClock/DigitalClock.jsx";
+let App = () => {
   return (
-    <div >
-      <CovidData/>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/digital" component={DigitalClock} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={ContactApp} />
+        </Switch>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
