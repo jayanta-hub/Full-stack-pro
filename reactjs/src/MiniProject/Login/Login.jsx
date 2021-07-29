143182d505778a98e8bef7e50ee847e46a18dbef
jayanta-hub <jayanta.garu@gmail.com>
2021-07-29 14:48:15 +0530

    react

--- reactjs/src/MiniProject/Login/Login.jsx
+++ reactjs/src/MiniProject/Login/Login.jsx
@@ -2,13 +2,41 @@ import React from "react"
 
 const Login = () => {
     return (
-        <>
-        <form action="">
-        <div>
-        
-        </div>
+        <React.Fragment>
+        <form>
+        <div className="login-reg-panel">
+		<div className="login-info-box">
+			<h2>Have an account?</h2>
+			<p>Lorem ipsum dolor sit amet</p>
+			<label id="label-register" for="log-reg-show">Login</label>
+			<input type="radio" name="active-log-panel" id="log-reg-show"  checked="checked"/>
+		</div>
+							
+		<div className="register-info-box">
+			<h2>Don't have an account?</h2>
+			<p>Lorem ipsum dolor sit amet</p>
+			<label id="label-login" for="log-login-show">Register</label>
+			<input type="radio" name="active-log-panel" id="log-login-show"/>
+		</div>
+							
+		<div className="white-panel">
+			<div className="login-show">
+				<h2>LOGIN</h2>
+				<input type="text" placeholder="Email"/>
+				<input type="password" placeholder="Password"/>
+				<input type="button" value="Login"/>
+			</div>
+			<div className="register-show">
+				<h2>REGISTER</h2>
+				<input type="text" placeholder="Email"/>
+				<input type="password" placeholder="Password"/>
+				<input type="password" placeholder="Confirm Password"/>
+				<input type="button" value="Register"/>
+			</div>
+		</div>
+	</div>
         </form>
-        </>
+        </React.Fragment>
     )
 }
 export default Login;
