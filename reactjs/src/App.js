9191055fdb4e27629d99fae63d15b1ac9cb3946a
jayanta-hub <jayanta.garu@gmail.com>
2021-08-04 12:35:07 +0530

    props

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,32 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import About from "./Hooks/Component/about.jsx";
-import Navbar from "./Hooks/NavBar/Navbar.jsx";
-import ContactApp from "./Hooks/ContactApp/contactapp.jsx";
-import DigitalClock from "./Hooks/DigitalClock/DigitalClock.jsx";
-import Message from "./Message/message.jsx"
-import hook from "./Hooks/react-useref-hook/hook.jsx"
-import Cart from "./MiniProject/Cart/Cart.jsx"
-import { Provider } from "react-redux";
-import { store } from "./Redux/Store";
+import React from 'react'
+import CompA from "./components/Func-Func/CompA.jsx"
 let App = () => {
-  return (
-    <>
-    <Provider store={store}>
-      <Router>  
-        <Navbar />
-        <Switch>
-          <Route exact path="/digital" component={DigitalClock} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={ContactApp} />
-          <Route exact path="/message" component={Message} />
-          <Route exact path="/hook" component={hook} />
-        </Switch>
-        <Cart/>
-      </Router>
-      </Provider>
-    </>
-  );
-};
-
-export default App;
+    return (
+      <>
+     <CompA/>
+      </>
+    );
+  };
+  
+  export default App;
--- reactjs/src/components/Func-Func/CompB.jsx
+++ reactjs/src/components/Func-Func/CompB.jsx
@@ -1,9 +1,11 @@
 function CompB(props){
 return (
     <>
+    <h2>CompB</h2>
     <h1>Hello:{props.one}</h1>
     <pre>{JSON.stringify(props)}</pre>
     <hr />
+
     <h4>Product Name: {props.four.productName}</h4>
     </>
 );
